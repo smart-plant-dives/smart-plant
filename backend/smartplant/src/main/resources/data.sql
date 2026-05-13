ca9ef290bdec7fa92f00b333ed11a4f46cfc40af
isabellaminhao <dev.isabella.minhao@gmail.com>
2026-05-13 14:03:09 -0300

    Correção do application.properties

--- backend/smartplant/src/main/resources/data.sql
+++ backend/smartplant/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert ignore into tb_usuario (id, nome_usuario, email, senha) values 
-(1, "isaminhao", "isabellaminhao@gmail.com", "bella123"); 
+(4, "isaminhao", "isabellaminhao@gmail.com", "bella123"); 
 
 insert ignore into tb_usuario (id, nome_usuario, email, senha) values 
 (2, "maribenini", "mariabenini@gmail.com", "morglove"); 
