a1b5e6e6b65cf762e8136e3c6ee56a031d7832ad
leticiagomes <dev.leticia.nascimento@gmail.com>
2026-06-03 12:47:20 -0300

    ...

--- backend/smartplant/src/main/java/br/com/smartplant/api/services/PlantaService.java
+++ backend/smartplant/src/main/java/br/com/smartplant/api/services/PlantaService.java
@@ -1,5 +1,6 @@
 package br.com.smartplant.api.services;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,39 +15,73 @@ public class PlantaService {
 	@Autowired
 	private PlantaRepository repository;
 	
-	
-	public List<Planta> listarTodos(){
-		return repository.findAll();
-	}
-	
-	public Planta buscarPorID(Long id) {
-		Planta planta = repository.findById(id).orElse(null);
-		
-		return planta;
-	}
-	
 	public Planta salvar(Planta planta) {
-		return repository.save(planta);
-	}
 		
-	
-		public Planta atualizar(Long id, Planta pNova) {
-		    Planta pVelha = repository.findById(id).get();
+		//EVITA PRODUTOS DUPLICADOS
+        Planta plantaExistente = PlantaRepository.findByNome(planta.getNomePlanta());
+        
+        if (plantaExistente != null) {
+            throw new RuntimeException("Já existe um produto com esse nome");
+        }
+        
+        return PlantaRepository.save(planta);
+    }
 
-		    pVelha.setNomePlanta(pNova.getNomePlanta());
-		    pVelha.setEspecie(pNova.getEspecie());
-		    pVelha.setNomeCategoria(pNova.getNomeCategoria());
+    public List<Planta> listarTodos() {
+        return repository.findAll();
+    }
 
-		    return repository.save(pVelha);
-		
-	}
-		
-		public String deletaR(Long id) {
-			Planta planta = buscarPorID(id);
-			
-			repository.delete(planta);
-			
-			return "Planta com o ID" + id + "foi excluído";
-		}
-	
+    public Planta buscarPorId(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Categoria com o ID" + id + " não encontrado."));
+    }
+
+    public Planta atualizar(Long id, Planta plantaAtualizada) {
+
+        Planta plantaAntiga = buscarPorId(id);
+
+        if (plantaAtualizada.getNomePlanta() != null  {
+            Planta.setNomePlanta(plantaAtualizada.getNomePlanta());
+        }
+
+        if (plantaNova.getEspecie() != null && plantaNova.getEspecie().isBlank()) {
+            plantaAntiga.getEspecie(plantaNova.getEspecie());
+        }
+
+        if (produtoNovo.getCategoria() != null) {
+            produtoAntigo.setCategoria(produtoNovo.getCategoria());
+        }
+
+        if (produtoNovo.getDisponivel() != null && produtoNovo.getDisponivel()) {
+            produtoAntigo.setDisponivel(produtoNovo.getDisponivel());
+        }
+
+        if (produtoNovo.getEstoqueMinimo() != null) {
+            produtoAntigo.setEstoqueMinimo(produtoNovo.getEstoqueMinimo());
+        }
+        if (produtoNovo.getImagem() != null && produtoNovo.getImagem().isBlank()) {
+            produtoAntigo.setImagam(produtoNovo.getImagem());
+        }
+
+        if (produtoNovo.getPreco() != null) {
+            if (produtoNovo.getPreco().compareTo(BigDecimal.ZERO) < 0) {
+                throw new RuntimeException("O preço não pode ser negativo");
+            }
+            produtoAntigo.setPreco(produtoNovo.getPreco());
+        }
+
+        if (produtoNovo.getQuantidadeEstoque() != null) {
+            produtoAntigo.setQuantidadeEstoque(produtoNovo.getQuantidadeEstoque());
+        }
+
+        return repository.save(produtoAntigo);
+
+    }
+
+    public void deletar(Long id) {
+        Planta planta = buscarPorId(id);
+
+        repository.delete(planta);
+    }
+
 }
